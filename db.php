c36db3955bf3ab9674a7d0bb18763966c0847e5e
manishkrpatel <103317099+manishkrpatel@users.noreply.github.com>
2022-04-20 17:08:45 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,5 @@
+<?php
+// $con = mysqli_connect("localhost","root","","sourcecodester_hoteldb") or die(mysql_error());
+$con = mysqli_connect("localhost","root","","hotel.db") or die(mysql_error());
+
+?>
